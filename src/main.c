6673a51590c7a01f6b18de3cb71a237973ba6311
Paulo Santana <paulo.santana.r@gmail.com>
2021-07-26 01:21:16 -0300

    feat: generate *game_info

--- src/main.c
+++ src/main.c
@@ -12,12 +12,26 @@
 
 #include "so_long.h"
 
+t_game_info	*parse_arg(char *args[])
+{
+	t_game_info	*gi;
+
+	gi = malloc(sizeof(t_game_info));
+	if (gi == NULL)
+		return (NULL);
+	gi->program_name = args[0];
+	return (gi);
+}
+
 int	main(int argc, char *argv[])
 {
-	int	args_valid;
+	int			args_valid;
+	t_game_info	*game_info;
 
 	args_valid = validate_args(argc, argv);
 	if (!args_valid)
 		handle_error(ERR_INVALID_ARGS);
-	ft_putstr_fd("args valid :)\n", 1);
+	game_info = parse_arg(argv);
+	printf("the program name is '%s'\n", game_info->program_name);
+	free(game_info);
 }
--- src/so_long.h
+++ src/so_long.h
@@ -14,16 +14,22 @@
 # include <stdio.h>
 # include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
+
 # define SO_LONG_H
 
 # define ERR_INVALID_ARGS 1
 
-struct s_x {
+struct s_game_info {
+	char	*program_name;
+};
+
+struct s_window {
 	void	*mlx_ptr;
 	void	*win_ptr;
 };
 
-typedef struct s_x	t_param;
+typedef struct s_window		t_window;
+typedef struct s_game_info	t_game_info;
 
 void	handle_error(int err);
 int		validate_args(int argc, char *argv[]);

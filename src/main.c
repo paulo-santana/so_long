e75f171615a9254b5a1916d8cd197ffa81815aa1
Paulo Santana <paulo.santana.r@gmail.com>
2021-08-06 19:31:47 -0300

    feat: connect to x server and start a window

--- src/main.c
+++ src/main.c
@@ -23,6 +23,35 @@ void	clear(t_game_info *game_info)
 	free(game_info);
 }
 
+int	key_handler(int keycode, void *param)
+{
+	(void) param;
+	printf("%d - %c\n", keycode, keycode);
+	return (0);
+}
+
+void	start_game(t_game_info *game_info)
+{
+	t_window	*window;
+
+	window = ft_calloc(sizeof(t_window), 1);
+	if (window == NULL)
+		quit_with_error(errno, game_info);
+	window->mlx_ptr = mlx_init();
+	if (window->mlx_ptr == NULL)
+		quit_with_error(errno, game_info);
+	window->win = mlx_new_window(window->mlx_ptr, 800, 600,
+			game_info->program_name);
+	if (window->win == NULL)
+		quit_with_error(errno, game_info);
+	mlx_key_hook(window->win, key_handler, game_info);
+	mlx_loop(window->mlx_ptr);
+	mlx_destroy_window(window->mlx_ptr, window->win);
+	mlx_destroy_display(window->mlx_ptr);
+	free(window->mlx_ptr);
+	free(window);
+}
+
 int	main(int argc, char *argv[])
 {
 	int			args_valid;
@@ -32,5 +61,6 @@ int	main(int argc, char *argv[])
 	if (!args_valid)
 		quit_with_error(errno, NULL);
 	game_info = parse_arg(argv);
+	start_game(game_info);
 	clear(game_info);
 }

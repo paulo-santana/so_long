8528a0bfb8134d95bf8afad87df670e845a2b8e6
Paulo Santana <paulo.santana.r@gmail.com>
2021-08-10 16:42:00 -0300

    refactor: calculate offset factors outside loop

--- src/map_renderer.c
+++ src/map_renderer.c
@@ -46,16 +46,20 @@ static void	copy_sprite_to_img(t_tile *tile, t_image_data *sprite)
 {
 	int	x;
 	int	y;
+	int	down_factor;
+	int	right_factor;
 
 	y = -1;
+	right_factor = tile->img_data.width * (tile->index % 6);
+	down_factor = sprite->width * 32 * (tile->index / 6);
 	while (++y < tile->img_data.height)
 	{
 		x = -1;
 		while (++x < tile->img_data.width)
 		{
 			((int *)tile->img_data.img)[x + (y * tile->img_data.width)]
-				= ((int *)sprite->img)[
-				x + tile->img_data.width * (tile->index) + sprite->width * y
+				= ((int *)sprite->img)[x + (right_factor)
+				+ (sprite->width * y + down_factor)
 			];
 		}
 	}
@@ -63,18 +67,25 @@ static void	copy_sprite_to_img(t_tile *tile, t_image_data *sprite)
 
 void	init_map(t_game_state *state)
 {
-	t_image_data	*map_data;
+	t_tile			*tile;
 
-	state->textures.collectible.index = 8;
+	state->textures.collectible.index = 29;
 	state->textures.exit.index = 23;
 	state->textures.floor.index = 7;
 	state->textures.wall.index = 1;
 	get_map_sprites(state);
-	map_data = &state->map.img_data;
-	get_mlx_image(state, &state->textures.collectible.img_data, 32, 32);
-	copy_sprite_to_img(&state->textures.collectible, &state->map.sprites_data);
-	mlx_put_image_to_window(state->mlx.mlx_ptr, state->mlx.window,
-		state->textures.collectible.img_data.img_ptr, 100, 120);
+	tile = &state->textures.wall;
+	get_mlx_image(state, &tile->img_data, 32, 32);
+	copy_sprite_to_img(tile, &state->map.sprites_data);
+	tile = &state->textures.collectible;
+	get_mlx_image(state, &tile->img_data, 32, 32);
+	copy_sprite_to_img(tile, &state->map.sprites_data);
+	tile = &state->textures.floor;
+	get_mlx_image(state, &tile->img_data, 32, 32);
+	copy_sprite_to_img(tile, &state->map.sprites_data);
+	tile = &state->textures.exit;
+	get_mlx_image(state, &tile->img_data, 32, 32);
+	copy_sprite_to_img(tile, &state->map.sprites_data);
 }
 
 int	draw_map(t_game_state *state)
@@ -86,5 +97,13 @@ int	draw_map(t_game_state *state)
 		already_initialized_map = 1;
 		init_map(state);
 	}
+	mlx_put_image_to_window(state->mlx.mlx_ptr, state->mlx.window,
+		state->textures.wall.img_data.img_ptr, 100, 120);
+	mlx_put_image_to_window(state->mlx.mlx_ptr, state->mlx.window,
+		state->textures.floor.img_data.img_ptr, 100, 120 + 32);
+	mlx_put_image_to_window(state->mlx.mlx_ptr, state->mlx.window,
+		state->textures.collectible.img_data.img_ptr, 100, 120 + 32);
+	mlx_put_image_to_window(state->mlx.mlx_ptr, state->mlx.window,
+		state->textures.exit.img_data.img_ptr, 100 + 32, 120 + 32);
 	return (0);
 }
